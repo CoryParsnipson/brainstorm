531d64b2d9d6bff93372c5e423812fdb49c4ff70
Cory Parsnipson <cparsnipson@gmail.com>
2014-10-31 18:08:47 -0700

    first commit, still going with keyring class. Moved path variables out of django settings into paths file. Added common python file.

---
+++ paths.py
@@ -0,0 +1,7 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(__file__)
+
+# add all user application paths to the project
+# sys.path.insert(0, BASE_DIR)
