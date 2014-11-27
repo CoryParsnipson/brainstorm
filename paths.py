ebd9c3838bb3b0ad21c49810bab0a92544df38f8
Cory Parsnipson <cparsnipson@gmail.com>
2014-11-26 18:41:18 -0800

    got rid of app subdirectory because it messes up Django. If I don't add in an __init__.py file in app, I can't run unit tests with just 'python manage.py test' and if I do I get some runtime model registry issue...

--- paths.py
+++ paths.py
@@ -11,7 +11,6 @@ VENDOR_DIR = os.path.join(BASE_DIR, "vendor")
 VENDOR_SWAGGER_DIR = os.path.join(VENDOR_DIR, "django-rest-swagger")
 
 # add all user application paths to the project
-# sys.path.insert(0, BASE_DIR)
 sys.path.insert(0, APP_DIR)
 sys.path.insert(0, VENDOR_DIR)
 sys.path.insert(0, VENDOR_SWAGGER_DIR)
