e3b41c5ee9c42e970799c1c22cee477cc0cce98f
Cory Parsnipson <cparsnipson@gmail.com>
2015-02-16 12:17:14 -0800

    removed DRF and swagger. not using them right now

--- blog/urls.py
+++ blog/urls.py
@@ -7,12 +7,6 @@ from rest_framework import routers
 
 from . import views
 
-# Use DRF router for RESTful API url generation
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'ideas', views.IdeaViewSet)
-router.register(r'thoughts', views.ThoughtViewSet)
-
 urlpatterns = patterns('',
     # favicon
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
@@ -39,16 +33,9 @@ urlpatterns = patterns('',
 
     url(r'^ideas/(?P<idea_slug>[a-z0-9\-]+)/(?P<thought_slug>[a-z0-9\-]+)/', views.thought_detail, name='thought-page'),
 
-    # RESTful api urls (it is very important that this app has no namespace...)
-    url(r'^api/', include(router.urls)),
-
-    # not part of DRF, but part of API, see views.py "Miscellaneous API"
     url(r'^api/login/', views.login, name='login'),
     url(r'^api/logout/', views.logout, name='logout'),
     url(r'^api/upload/', views.upload, name='upload'),
-
-    url(r'^api/docs/', include('rest_framework_swagger.urls')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     url(r'^api/forms/idea/', views.FormIdeaView.as_view(), name='forms-idea'),
     url(r'^api/forms/thought/', views.FormThoughtView.as_view(), name='forms-thought'),
