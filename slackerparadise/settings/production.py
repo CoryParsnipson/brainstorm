ca439fdcc796a951ad82c101d936c85a19733170
Cory Parsnipson <cparsnipson@gmail.com>
2015-03-02 01:19:06 -0800

    production settings change

--- slackerparadise/settings/production.py
+++ slackerparadise/settings/production.py
@@ -19,7 +19,7 @@ import paths
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = int(os.environ['DJANGO_SECRET_KEY'])
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
