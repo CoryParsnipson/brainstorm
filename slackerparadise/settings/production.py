01e68e3025626dcd43a44f1f90c6fa4f19dd7234
Cory Parsnipson <cparsnipson@gmail.com>
2015-03-03 03:04:03 -0800

    ugh

--- slackerparadise/settings/production.py
+++ slackerparadise/settings/production.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 
 import dj_database_url  # heroku postgres adapter
+from storages.backends.s3boto import S3BotoStorage
 
 import paths
 
@@ -96,6 +97,9 @@ AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 
+StaticS3BotoStorage = lambda: S3BotoStorage(location=paths.STATIC_ROOT)
+MediaS3BotoStorage = lambda: S3BotoStorage(location=paths.MEDIA_DIR)
+
 # media files
 MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_ROOT = paths.MEDIA_DIR
@@ -107,7 +111,8 @@ paths.MEDIA_VIDEO_ROOT = os.path.join(paths.MEDIA_ROOT, paths.MEDIA_VIDEO_DIR)
 paths.MEDIA_FILE_ROOT = os.path.join(paths.MEDIA_ROOT, paths.MEDIA_FILE_DIR)
 
 #DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
-DEFAULT_FILE_STORAGE = "slackerparadise.blog.s3utils.MediaS3BotoStorage"
+#DEFAULT_FILE_STORAGE = "slackerparadise.blog.s3utils.MediaS3BotoStorage"
+DEFAULT_FILE_STORAGE = MediaS3BotoStorage
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
@@ -116,7 +121,8 @@ STATIC_ROOT = '/' + paths.STATIC_ROOT + '/'
 paths.STATIC_DIR = STATIC_URL
 
 #STATICFILES_STORAGE = "storages.backends.s3boto.S3BotoStorage"
-STATICFILES_STORAGE = "slackerparadise.blog.s3utils.StaticS3BotoStorage"
+#STATICFILES_STORAGE = "slackerparadise.blog.s3utils.StaticS3BotoStorage"
+STATICFILES_STORAGE = StaticS3BotoStorage
 
 # Template dirs
 # keep global, non-app specific template files here
