55eb8c2481a18a5a8350eab5ebc9685bc5be1303
Cory Parsnipson <cparsnipson@gmail.com>
2015-03-03 03:22:51 -0800

    eeergghghghh

--- slackerparadise/settings/production.py
+++ slackerparadise/settings/production.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 
 import dj_database_url  # heroku postgres adapter
-from storages.backends.s3boto import S3BotoStorage
 
 import paths
 
