3a89bc9ff32e3babc6127e811f94394cf8534ba4
Cory Parsnipson <cparsnipson@gmail.com>
2015-03-03 23:19:43 -0800

    changing s3boto storage parameters

--- slackerparadise/s3utils.py
+++ slackerparadise/s3utils.py
@@ -2,5 +2,14 @@ from storages.backends.s3boto import S3BotoStorage
 
 import paths
 
-StaticS3BotoStorage = lambda: S3BotoStorage(location=paths.STATIC_ROOT)
-MediaS3BotoStorage = lambda: S3BotoStorage(location=paths.MEDIA_DIR)
+StaticS3BotoStorage = lambda: S3BotoStorage(
+    location=paths.STATIC_ROOT,
+    secure_urls=False,
+    default_acl='public-read-write',
+)
+
+MediaS3BotoStorage = lambda: S3BotoStorage(
+    location=paths.MEDIA_DIR,
+    secure_urls=False,
+    default_acl='public-read-write',
+)
