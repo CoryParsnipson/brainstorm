9f9a0a171ad67035934810e9d6f99004918d0df0
Cory Parsnipson <cparsnipson@gmail.com>
2015-03-04 19:50:27 -0800

    media file serving and upload is working. final production changes

--- slackerparadise/settings/production.py
+++ slackerparadise/settings/production.py
@@ -22,13 +22,14 @@ import paths
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = False
-DEBUG = True
+DEBUG = False
 
-#TEMPLATE_DEBUG = False
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '.slackerparadise.com',
+    '.slackerparadise.herokuapp.com'
+]
 
 # Application definition
 INSTALLED_APPS = (
